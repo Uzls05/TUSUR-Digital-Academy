0f3e18cfc85314561dc811a056f412c636608d99
Uzls05 <s-podkr@yandex.ru>
2024-03-19 22:38:24 +0700

    Add task 8.1 and txt file

--- Task 8.1.py
+++ Task 8.1.py
@@ -1,1 +1,10 @@
-print("Hello")
+elements_list = list(input("Введите элементы списка без пробела: \n"))
+
+not_unic_elements = [
+    x for x in elements_list
+    if elements_list.count(x) > 1
+]
+
+print(not_unic_elements)
+
+input()
