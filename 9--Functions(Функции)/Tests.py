c644d911c0d2205f486037b46df6383d30551af3
Uzls05 <s-podkr@yandex.ru>
2024-03-20 02:14:33 +0700

    asd

--- 9--Functions(Функции)/Tests.py
+++ 9--Functions(Функции)/Tests.py
@@ -12,5 +12,20 @@ def print_max(a,b):
         print(f"{b},is max")
 
 
+def get_max_value(a, b):
+    if a > b:
+        return a
+    elif a == b:
+        return None
+    return b
+
+
 say_hi("Man")
 print_max(6,6)
+a = 1
+b = 1
+max_value = get_max_value(a, b)
+if max_value is None:
+    print(f"{a},equals to {b}")
+else:
+    print(f"{max_value} is max")
